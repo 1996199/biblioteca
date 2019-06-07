ec94ff140d193acec95c5397f4ed7dd8e05c18fc
Gamaliel <GamalielGnz96@Gmail.com>
2019-06-06 23:23:38 -0500

    klj

--- conexion.php
+++ conexion.php
@@ -5,6 +5,7 @@
 		private function __construct (){}
  
 		public static function conectar(){
+			echo('hola');
 			$pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
 			self::$conexion= new PDO('mysql:biblioteca123-mysqldbserver.mysql.database.azure.com=localhost;dbname=mysqldatabase32833','mysqldbuser@biblioteca123-mysqldbserver','GARCIAnu1996',$pdo_options);
 			return self::$conexion;
